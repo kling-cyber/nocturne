9ca175a60b343b6a2620047f9f5fcaee517aac2e
Kavish <114940209+kling-cyber@users.noreply.github.com>
2026-09-04 13:10:57 +0530

    Update config.js

--- nocturne-vercel-3.5/frontend/config.js
+++ nocturne-vercel-3.5/frontend/config.js
@@ -1,3 +1,3 @@
 // Set this to the public URL of the NOCTURNE game server.
 // Example: https://nocturne-api.example.com
-window.NOCTURNE_SERVER_URL = "https://YOUR-NOCTURNE-BACKEND.example.com";
+window.NOCTURNE_SERVER_URL = "https://nocturne-8tko.onrender.com";
